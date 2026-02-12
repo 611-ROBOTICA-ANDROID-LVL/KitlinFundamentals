05bd36b103b6c0fadaacec7141a51a1a1f135e96
Loenthgreen <villagomezloenthgreen@gmail.com>
2026-02-11 20:47:46 -0600

    Inicio de proyecto

---
+++ app/src/main/java/com/pjasoft/kotlinfundamentals/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.pjasoft.kotlinfundamentals
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pjasoft.kotlinfundamentals.ui.theme.KotlinFundamentalsTheme
+
+
+//variables INMUTABLES
+val name : String = "Loenthgreen"
+
+//Variable INMUTABLE
+var age : Int = 28
+var ages : int? = null
+
+ages 29
+
+
+
+//Tipos de Datos: int, string, double, float, char, array, boolean
+val is01d : Boolean = false
+val height : Double = 1.65
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KotlinFundamentalsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KotlinFundamentalsTheme {
+        Greeting("Android")
+    }
+}
