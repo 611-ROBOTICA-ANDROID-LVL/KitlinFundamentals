9a4af2503eb134fcaf3580f1e56d4f1cb59c7a66
Loenthgreen <villagomezloenthgreen@gmail.com>
2026-02-20 17:33:52 -0600

    Ejercicios

--- app/lib/src/main/java/com/example/lib/01-Variables_Loenthgreen.kt
+++ app/lib/src/main/java/com/example/lib/01-Variables_Loenthgreen.kt
@@ -6,7 +6,7 @@ fun main() {
     val estatura: Double = 1.78
     val esEstudiante: Boolean = true
 
-    // TODO: Usa println con string templates para imprimir el mensaje
+
     println("Me llamo $nombre, tengo $edad de edad, mido $estatura m y es $esEstudiante que soy estudiante")
 }
 //Funcionalidad 2 para git
--- app/lib/src/main/java/com/example/lib/07_Contador_Loenthgreen.kt
+++ app/lib/src/main/java/com/example/lib/07_Contador_Loenthgreen.kt
@@ -1,2 +1,31 @@
 package com.example.lib
 
+
+fun main() {
+    val palabra = "externocleidomastuideo"
+
+    var a = 0
+    var e = 0
+    var i = 0
+    var o = 0
+    var u = 0
+
+
+    for (c in palabra.lowercase()) {
+        when (c) {
+            'a' -> a++
+            'e' -> e++
+            'i' -> i++
+            'o' -> o++
+            'u' -> u++
+
+        }
+    }
+
+    println("Palabra: \"$palabra\"")
+    println("a: $a")
+    println("e: $e")
+    println("i: $i")
+    println("o: $o")
+    println("u: $u")
+}
