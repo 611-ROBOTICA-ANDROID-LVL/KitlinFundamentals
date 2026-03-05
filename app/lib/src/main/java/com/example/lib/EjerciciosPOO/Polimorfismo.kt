19a177e015730506673dd60860d44428d707bfd4
Loenthgreen <villagomezloenthgreen@gmail.com>
2026-03-04 19:16:48 -0600

    Ejercicio Polimorfismo

--- app/lib/src/main/java/com/example/lib/EjerciciosPOO/Polimorfismo.kt
+++ app/lib/src/main/java/com/example/lib/EjerciciosPOO/Polimorfismo.kt
@@ -1,2 +1,36 @@
 package com.example.lib.EjerciciosPOO
 
+abstract class Vehiculo (val marca: String, val velocidadMaxima: Int) {
+    abstract  fun tipoVehiculo (): String
+
+    fun mostrarInfo () {
+        println ("${tipoVehiculo()} : $marca - Vel.  max: $velocidadMaxima km/h")
+
+    }
+}
+
+class Auto (marca: String, velMax: Int) : Vehiculo (marca, velMax) {
+    override fun tipoVehiculo()= "Auto"
+
+}
+
+class Moto (marca: String, velMax: Int) : Vehiculo (marca, velMax) {
+    override fun tipoVehiculo() = "Moto"
+}
+
+class Camion (marca: String, velMax: Int) : Vehiculo (marca, velMax) {
+    override fun tipoVehiculo() = "Camion"
+}
+
+
+    fun main () {
+    val vehiculos: List <Vehiculo> = listOf (
+        Auto ("Lamborgini" , 380) ,
+        Moto ("Italika" , 220) ,
+        Camion ("Volvo" , 120)
+    )
+        for (i in vehiculos) {
+            i.mostrarInfo()
+        }
+}
+
